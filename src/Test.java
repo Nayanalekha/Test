a3f802438caabffb7a5ac0f1e9b4d6fda4c48db4
Nayanalekha <38463625+Nayanalekha@users.noreply.github.com>
2022-04-21 14:57:35 +1200

    3rd from Jidea

--- src/Test.java
+++ src/Test.java
@@ -2,9 +2,11 @@ public class Test {
 
     public static void main(String arg[]){
         System.out.println(" Hello " + arg[0]);
+        isPrint();
     }
     
-    private boolean isprint(){
+    private static boolean isPrint(){
         System.out.print("True");
+        return true;
     }
 }
