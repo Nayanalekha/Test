8199556826cdaf72d0f8bab054be88aed12d63c5
Nayanalekha <38463625+Nayanalekha@users.noreply.github.com>
2022-04-21 14:53:45 +1200

    Update Test.java
    2nd commit

--- src/Test.java
+++ src/Test.java
@@ -3,4 +3,8 @@ public class Test {
     public static void main(String arg[]){
         System.out.println(" Hello " + arg[0]);
     }
+    
+    private boolean isprint(){
+        System.out.print("True");
+    }
 }
